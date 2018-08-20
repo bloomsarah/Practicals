d13eb709ccdd65cb5595ffe55f54e2196cd36d93
Sarah <sarah.bloom@my.jcu.edu.au>
2018-08-20 15:10:59 +1000

    Add password entry to practical folder

---
+++ prac_03/password_entry.py
@@ -0,0 +1,11 @@
+MIN_LENGTH = 10
+
+password = input("Enter a password: ")
+
+if len(password) < MIN_LENGTH:
+    print("Invalid password!")
+    password = input("Enter a password: ")
+else:
+    for char in password:
+        print('*', end=' ')
+print()
