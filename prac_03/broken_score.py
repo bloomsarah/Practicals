6d6de7910ed4d85941a9066272da18009cca3773
sarah.bloom@my.jcu.edu.au <Neofibularia7>
2018-08-21 13:53:13 +1000

    Finish editing broken score function.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -5,20 +5,22 @@ def main():
     """
 
     score = float(input("Enter score: "))
+
     result = score_calculator(score)
+
     print(result)
 
-    def score_calculator(score):
-        if score < 0 or score > 100:
-            result = "Invalid"
-        elif score >= 90:
-            result = "Excellent"
-        elif score >= 50 and score < 90:
-            result = "Passable"
-        else:
-            result = "Bad"
-        return result
 
+def score_calculator(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50 and score < 90:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
 
 
 main()
