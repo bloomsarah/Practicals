e78a974e77f48480b326090b8650aa737eb8b91c
sarah.bloom@my.jcu.edu.au <Neofibularia7>
2018-08-21 15:39:55 +1000

    Add quick picks lottery generator, unfinished.

---
+++ prac_04/lottery_ticket_generator.py
@@ -0,0 +1,7 @@
+number_of_picks = int(input("Enter number of quick picks: "))
+
+# generate n lines of output
+# each line has 6 random numbers between 1 and 45
+# no repeated numbers on each line
+# display numbers in sorted (ascending) order
+# do not use the random sample() fxn
