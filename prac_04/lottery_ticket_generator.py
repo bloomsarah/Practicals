ea2d9d8269f4ce884a1be4f68765f83d785e217b
sarah.bloom@my.jcu.edu.au <Neofibularia7>
2018-08-28 13:37:18 +1000

    Add complete working lottery generator.

--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -1,7 +1,32 @@
-number_of_picks = int(input("Enter number of quick picks: "))
+"""
+Generate program that asks for number of quick picks
+print that number of lines, with 6 numbers per line
+numbers must be sorted and between 1 and 45
+"""
 
-# generate n lines of output
-# each line has 6 random numbers between 1 and 45
-# no repeated numbers on each line
-# display numbers in sorted (ascending) order
-# do not use the random sample() fxn
+import random
+
+NUMBERS_PER_LINE = 6
+MIN = 1
+MAX = 45
+
+
+def main():
+
+    number_of_picks = int(input("Enter number of picks: "))
+    while number_of_picks < 0:
+        print("Invalid number of picks.")
+        number_of_picks = int(input("Enter number of picks: "))
+
+    for i in range(number_of_picks):
+        pick = []
+        for j in range(NUMBERS_PER_LINE):
+            number = random.randint(MIN, MAX)
+            while number in pick:
+                number = random.randint(MIN, MAX)
+            pick.append(number)
+        pick.sort()
+        print(" ".join("{:2}".format(number) for number in pick))
+
+
+main()
