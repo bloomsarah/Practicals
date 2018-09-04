35bea6e033e095c0903cc96d5d451e0ca058c13f
Sarah Bloom <42033603+bloomsarah@users.noreply.github.com>
2018-09-04 13:48:30 +1000

    Update word_occurences.
    Delete if/else loop based on Brandon's suggestions.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,12 +7,9 @@ word_to_count = {}
 
 words = input("Text: ")
 
-for word in words:
-    if word in word_to_count:
-        word_to_count[word] += 1
-    else:
-        word_to_count[word] = 1
-
+frequency = word_to_count.get(word, 0)
+unique_words[word] = frequency + 1       
+        
 for key, value in sorted(word_to_count.items()):
     print("{} : {}".format(key, value))
 
