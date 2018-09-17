3a65bf59af4d944d4f83c89595bcb17eda5d0ac5
Sarah <sarah.bloom@my.jcu.edu.au>
2018-09-17 14:50:19 +1000

    Add finished version of programming language files.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,8 +1,23 @@
+"""CP1404 Prac 6: Intermedite Exercise"""
+
+
 from prac_06.programming_language import ProgrammingLanguage
 
 
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+def main():
 
-# need to add 3 other programming languages but the image is not available on my computer
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
+
+
+
