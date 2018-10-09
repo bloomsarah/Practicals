72090e6c6342d3cd4e8bb0a8cef9581ddba75cfb
Sarah <sarah.bloom@my.jcu.edu.au>
2018-10-09 11:33:51 +1100

    Add taxi test file fully functioning.

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,15 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    """Test for the Taxi class"""
+    prius = Taxi("Prius 1", 100, 1.23)
+    prius.drive(40)
+    print(prius)
+    print("Fare = $", prius.get_fare())
+    prius.start_fare()
+    print(prius)
+    print("Fare = $", prius.get_fare())
+
+
+main()
