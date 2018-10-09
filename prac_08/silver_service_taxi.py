8ab48c29fcd0b4899ae67f04394568f5520ca375
Sarah <sarah.bloom@my.jcu.edu.au>
2018-10-09 12:22:58 +1100

    Add test file to ensure that Silver Service Taxi class is working

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -1,5 +1,6 @@
 from prac_08.taxi import Taxi
 
+
 class SilverServiceTaxi(Taxi):
     flagfall = 4.50
 
---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,13 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    hummer = SilverServiceTaxi("Hummer", 100, 2)
+    print(hummer)
+    hummer.drive(18)
+    print(hummer)
+    print(hummer.get_fare())
+
+
+main()
+
