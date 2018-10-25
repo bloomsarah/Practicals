327d84f003df8ef721037fc729598df89ebceca7
jc345594 <sarah.bloom@my.jcu.edu.au>
2018-10-25 14:33:14 +1000

    Final commit

--- prac_10/recursion.py
+++ prac_10/recursion.py
@@ -19,8 +19,10 @@ print(do_it(5))
 def do_something(n):
     """Print the squares of positive numbers from n down to 0."""
     if n <= 0:
-        return 0
-    return n ** 2, do_something(n - 1)
+        print(0)
+    else:
+        print(n ** 2)
+        do_something(n - 1)
 
 # TODO: 3. write down what you think the output of do_something(4) will be,
 # TODO: 4. use the debugger to step through and see what's actually happening
