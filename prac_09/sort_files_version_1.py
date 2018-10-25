af939e115213275f05580a3d3d2be02642389234
Sarah <sarah.bloom@my.jcu.edu.au>
2018-10-25 11:31:35 +1100

    Add loop that prints all of the extension types in the directory

--- prac_09/sort_files_version_1.py
+++ prac_09/sort_files_version_1.py
@@ -5,7 +5,7 @@ a list or a set as your process the files. """
 import os
 import shutil
 
-extensions = {}
+extensions = []
 os.chdir('FilesToSort')
 print(os.getcwd())
 print(os.listdir('.'))
@@ -13,4 +13,16 @@ print(os.listdir('.'))
 for filename in os.listdir('.'):
     p = "{}/{}".format(os.getcwd(), filename)
     extension = os.path.splitext(p)
-    print(extension[1])
+    extensions.append(extension[1])
+extensions_set = set(extensions)
+print(extensions_set)
+
+for extension in extensions_set:
+    os.mkdir(extension)
+
+for filename in os.listdir('.'):
+    p = "{}/{}".format(os.getcwd(), filename)
+    extension = os.path.splitext(p)
+    if extension[1]
+
+
